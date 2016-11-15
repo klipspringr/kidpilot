c4d214c48eb00d4f21367b0061ea0a949b82e71f
Teresa Ibarra <mtmibarra@gmail.com>
2016-11-14 22:29:17 -0600

    Add working code

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 import smtplib
-from google.cloud import vision
-import io
+game_start = True
+email_sent = False
 
 # NOTE: This is not meant to be a working demo. Rather, this is the show the interaction
 # between the application, XapiX, and 3rd party APIs.
@@ -10,72 +10,84 @@ import io
 #
 
 class AirBerlin:
-  def __init__(api_key):
+  def __init__(self, api_key):
     self.api_key = api_key # We don't actually use api_key, just mocking
     
-  def get_booking:
+  def get_booking(self):
     return {
       'status': 'success',
       'data': {
         # fake customer data used, parameters removed for readability
+        "customer_name": "Teresa Ibarra",
         "booking_id": "12345",
         "random_id": "12345",
-        "customer_addresses_0_email": "hello@google.com",
+        "customer_email_address": "hello@google.com",
         "booking_date": "11/10/2016",
         "record_locator": "A1234",
-        "passenger_type": "CHILD"
+        "passenger_type": "ADULT"
     }
   }
 
-  def flight_segments(record_locator):
+  def flight_segments(self,record_locator):
     return {
       "date": "12/10/16",
       "layover_time": 2.0 # added parameter for the sake of clarity. when used in real life,
-      # this flight_segments field can be used to calculate layover time based on when flights
-      # land and depart
+                          # this flight_segments field can be used to calculate layover time based on when flights
+                          # land and depart
     }
 
 berlin_api = AirBerlin("api_key")
 
 def get_layover_length():
-  return berlin_api.flight_segments("A1234").layover_time
+  return berlin_api.flight_segments("A1234")["layover_time"]
 
-for passenger in berlin_api.get_booking.passenger_type: 
-# look through all bookings for children. in real life, a better solution would be to run 
-# this code after a booking has been made.
-  if passenger == "CHILD":
-    # introduce customer to KidPilot by sending parent an email introducing the application
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()
-    server.login("service@airberlin.com", "password")
-     
-    msg = "We noticed that you're traveling with small children"
-    server.sendmail("service@airberlin.com", AirBerlin.get_booking.customer_addresses_0_email , msg)
-    server.quit()
+if berlin_api.get_booking()["data"]["passenger_type"] == "CHILD":
+  # look through bookings for traveling children. in real life, a better solution would be to run 
+  # this code every time after a booking has been made.
+
+  server = smtplib.SMTP('smtp.gmail.com', 587)
+  server.starttls()
+  server.login("service@airberlin.com", "password")
+  # introduce customer to KidPilot by sending parent an email introducing the KidPilot application
+   
+  msg = "We noticed that you're traveling with small children"
+  server.sendmail("service@airberlin.com", berlin_api.flight_segments("A1234")["customer_email_address"] , msg)
+  server.quit()
+  email_sent = True
 
 
 if game_start == True:
   # ideally, this code would be run on the mobile application once the user has clicked the start game button.
-  set_game_time_limit = get_layover_length % 10 # set the time limit to how many 10 minute chunks we can fit in the layover
+  set_game_time_limit = get_layover_length() % 10 # set the time limit to how many 10 minute chunks we can fit in the layover
 
-#
-# XAPIX INTERACTION WITH GOOGLE CLOUD IMAGE RECOGNITION API
-#
+print "NAME: " + berlin_api.get_booking()["data"]["customer_name"]
+print "LAYOVER LENGTH: " + str(get_layover_length())
+print "GAMEPLAY TIME: " + str(set_game_time_limit)
+print "EMAIL SENT? : " + str(email_sent)
 
-client = vision.Client()
+# #
+# # XAPIX INTERACTION WITH GOOGLE CLOUD IMAGE RECOGNITION API
+# #
 
-class GoogleCloudVision:
-  def __init__(api_key):
-    self.api_key = api_key # Again, we don't actually use api_key, just mocking
+# THIS HAS BEEN COMMENTED OUT SO THAT IT DOESN'T INTERFERE WITH RUNNING THE SCRIPT
+# It is here to serve the purpose of how the application would use the Google API.
 
-  def post_image(): # send over image to Google for recognition
-    image = client.image('./image.jpg')
-    landmarks = image.detect_landmarks()
-    response = landmarks[0].description # landmarks[0].description contains a verbal description of the landmark 
-                                        # found in the image. For example, a restaurant.
+# from google.cloud import vision
+# import io
 
-google_api = GoogleCloudVision("api_key")
+# client = vision.Client()
 
-if pictureTaken == True: # once a user has taken a picture, send it over to the Google Vision API for identification
-  google_api.post_image
-  return response
+# class GoogleCloudVision:
+#   def __init__(api_key):
+#     self.api_key = api_key # Again, we don't actually use api_key, just mocking
+
+#   def post_image(): # send over image to Google for recognition
+#     image = client.image('./sydney.jpg')
+#     landmarks = image.detect_landmarks()
+#     response = landmarks[0].description # landmarks[0].description contains a verbal description of the landmark 
+#                                         # found in the image. For example, a restaurant.
+
+# google_api = GoogleCloudVision("api_key")
+
+# if pictureTaken == True: # once a user has taken a picture, send it over to the Google Vision API for identification
+#   google_api.post_image
